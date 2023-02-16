6c010ecd5ca21ba9f7538b745c0e95ec974a2a1c
OFFICE\rmolyanov <rmolyanov@corp.finam.ru>
2023-02-16 15:12:50 +0300

    с выделением в отдельную процедуру

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
+    go_to_login_page(browser)
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-    
+    login_link.click()    
