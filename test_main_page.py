aafd2113788de38f180f535cdf1e85389430a60b
OFFICE\rmolyanov <rmolyanov@corp.finam.ru>
2023-02-18 16:50:29 +0300

    + guest_should_see_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -11,3 +11,8 @@ def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()    
     
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
