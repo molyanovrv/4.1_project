5139f82722978347742968a0fe325decdc795f94
OFFICE\rmolyanov <rmolyanov@corp.finam.ru>
2023-02-18 15:58:15 +0300

    1st PO test: can go to login page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,9 @@
-link = "http://selenium1py.pythonanywhere.com/"
-
 
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-    
+
     def open(self):
         self.browser.get(self.url)
         
--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,12 @@
-from .pages.main_page import MainPage
-from selenium.webdriver.common.by import By
-link = "http://selenium1py.pythonanywhere.com/"
+from pages.main_page import MainPage
+
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-
+    
 def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()    
